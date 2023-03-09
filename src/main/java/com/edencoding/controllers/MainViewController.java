4b5fd1f7160126fb5d173a1cc12daf6322735dab
luisandre-oliveira <luisandre12100@gmail.com>
2023-03-09 22:34:40 +0000

    UI pre new design overhaul

--- src/main/java/com/edencoding/controllers/MainViewController.java
+++ src/main/java/com/edencoding/controllers/MainViewController.java
@@ -11,17 +11,17 @@ import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 public class MainViewController implements Initializable {
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         
     }
 
     @FXML
-    private TextArea text;
+    private TextArea textArea;
 
     @FXML
     private Button submitButton;
@@ -31,8 +31,31 @@ public class MainViewController implements Initializable {
 
     @FXML
     private void handleSubmitButtonAction(ActionEvent event) {
-        if(!text.getText().isEmpty()) {
-            System.out.println(text.getText());
+        if(!textArea.getText().isEmpty() && integerCheck.isSelected()) { // if textarea has text and the checkbox is ticked
+            int randInt = (int) (Math.random() * 2000000) + 1;
+            System.out.println("Message: " + randInt);
+            textArea.setPromptText(String.valueOf(randInt));
+        }
+
+        else if(textArea.getText().isEmpty() && integerCheck.isSelected()) { // if textarea doesn't have text and the checkbox is ticked
+            System.out.println("Message: " + textArea.getText());
+            System.out.println("Does the user only want integers -> " + integerCheck.isSelected());
+        }
+
+        else if(textArea.getText().isEmpty() && !integerCheck.isSelected()) {  // if textarea doesn't have text and the checkbox is not ticked
+            // will print a random set of chars of a random size
+            int leftLimit = 65, rightLimit = 90; //ascii values from capital A to capital Z
+            int numChars = (int) (Math.random() * 10) + 1;
+
+            Random random = new Random();
+
+            String temp = random.ints(leftLimit, rightLimit + 1)
+                    .limit(numChars)
+                    .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                    .toString();
+
+            System.out.println(temp);
+            textArea.setPromptText(temp);
         }
         event.consume();
     }
@@ -47,9 +70,17 @@ public class MainViewController implements Initializable {
     private void handleGitButtonClicked(ActionEvent event) {
         new Application() {
             @Override
-            public void start(Stage stage) {
-            }
+            public void start(Stage stage) { }
         }.getHostServices().showDocument("https://github.com/joseluisgomes/PITI/");
+        event.consume();
+    }
+
+    @FXML
+    private void handleOnlyIntCheckAction(ActionEvent event) {
+        if(integerCheck.isSelected()) //println only here for testing purposes on the functionality of the checkbox
+            System.out.println("User only wants integers");
+        else
+            System.out.println("User wants all chars");
         event.consume();
     }
 }
