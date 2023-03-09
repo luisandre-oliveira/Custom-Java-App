1da3124b7998b5ea7419a08b9d0e7c85310c11de
luisandre-oliveira <luisandre12100@gmail.com>
2023-03-09 23:48:52 +0000

    Added what will become the history log of messages. UI is good, functionality is work in progress.

--- src/main/java/com/edencoding/controllers/MainViewController.java
+++ src/main/java/com/edencoding/controllers/MainViewController.java
@@ -2,12 +2,13 @@ package com.edencoding.controllers;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -24,39 +25,66 @@ public class MainViewController implements Initializable {
     private TextArea textArea;
 
     @FXML
-    private Button submitButton;
+    private CheckBox integerCheck;
+
+    private boolean intCheckFlag;
 
     @FXML
-    private CheckBox integerCheck;
+    private TableView historyTable;
+
+    private final ObservableList<String> data = FXCollections.observableArrayList();
 
     @FXML
     private void handleSubmitButtonAction(ActionEvent event) {
-        if(!textArea.getText().isEmpty() && integerCheck.isSelected()) { // if textarea has text and the checkbox is ticked
-            int randInt = (int) (Math.random() * 2000000) + 1;
-            System.out.println("Message: " + randInt);
-            textArea.setPromptText(String.valueOf(randInt));
+        int counter = 0;
+        String temp = "";
+
+        if(!textArea.getText().isEmpty() && !intCheckFlag) { // if textarea has text and the checkbox is not ticked
+            temp = textArea.getText();
         }
 
-        else if(textArea.getText().isEmpty() && integerCheck.isSelected()) { // if textarea doesn't have text and the checkbox is ticked
-            System.out.println("Message: " + textArea.getText());
-            System.out.println("Does the user only want integers -> " + integerCheck.isSelected());
-        }
-
-        else if(textArea.getText().isEmpty() && !integerCheck.isSelected()) {  // if textarea doesn't have text and the checkbox is not ticked
+        else if(textArea.getText().isEmpty() && !intCheckFlag) {  // if textarea doesn't have text and the checkbox is not ticked
             // will print a random set of chars of a random size
             int leftLimit = 65, rightLimit = 90; //ascii values from capital A to capital Z
             int numChars = (int) (Math.random() * 10) + 1;
 
             Random random = new Random();
 
-            String temp = random.ints(leftLimit, rightLimit + 1)
+            temp = random.ints(leftLimit, rightLimit + 1)
                     .limit(numChars)
                     .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                     .toString();
 
-            System.out.println(temp);
             textArea.setPromptText(temp);
         }
+
+        else if(intCheckFlag) { // if textarea doesn't have text and the checkbox is ticked
+            int randInt = (int) (Math.random() * 2000000) + 1;
+            temp = String.valueOf(randInt);
+
+            textArea.setPromptText(temp);
+        }
+
+        counter++;
+        System.out.println("Message: " + temp);
+
+        /*
+        historyTable.setEditable(true);
+
+        TableColumn numberColumn = new TableColumn("N");
+        numberColumn.setMinWidth(25);
+        numberColumn.setCellFactory(new PropertyValueFactory<Integer,String>("counter"));
+
+        TableColumn messageColumn = new TableColumn("Messages");
+        messageColumn.setMinWidth(300);
+        messageColumn.setCellFactory(new PropertyValueFactory<Integer,String>("temp"));
+
+        historyTable.setItems(data);
+        historyTable.getColumns().addAll(counter,temp);
+
+        data.add(temp);
+        */
+
         event.consume();
     }
 
@@ -77,10 +105,13 @@ public class MainViewController implements Initializable {
 
     @FXML
     private void handleOnlyIntCheckAction(ActionEvent event) {
-        if(integerCheck.isSelected()) //println only here for testing purposes on the functionality of the checkbox
+        if(integerCheck.isSelected()) { //println only here for testing purposes on the functionality of the checkbox
             System.out.println("User only wants integers");
-        else
+            intCheckFlag = true;
+        } else {
             System.out.println("User wants all chars");
+            intCheckFlag = false;
+        }
         event.consume();
     }
 }
