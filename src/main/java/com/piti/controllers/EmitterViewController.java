5452fdbbf9395b43cd8ea3d78ba22aa1203bb42c
luisandre-oliveira <luisandre12100@gmail.com>
2023-03-10 23:43:13 +0000

    Some files failed to be committed

---
+++ src/main/java/com/piti/controllers/EmitterViewController.java
@@ -0,0 +1,90 @@
+package com.piti.controllers;
+
+import com.piti.App;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Random;
+
+public class EmitterViewController {
+
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    private CheckBox integerCheck;
+
+    private boolean intCheckFlag;
+
+    @FXML
+    private void handleSubmitButtonAction(ActionEvent event) {
+        String temp = "";
+
+        if(!textArea.getText().isEmpty() && !intCheckFlag) { // if textarea has text and the checkbox is not ticked
+            temp = textArea.getText();
+        }
+
+        else if(textArea.getText().isEmpty() && !intCheckFlag) {  // if textarea doesn't have text and the checkbox is not ticked
+            // will print a random set of chars of a random size
+            int leftLimit = 65, rightLimit = 90; //ascii values from capital A to capital Z
+            int numChars = (int) (Math.random() * 10) + 1;
+
+            Random random = new Random();
+
+            temp = random.ints(leftLimit, rightLimit + 1)
+                    .limit(numChars)
+                    .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                    .toString();
+
+            textArea.setPromptText(temp);
+        }
+
+        else if(intCheckFlag) { // if textarea doesn't have text and the checkbox is ticked
+            int randInt = (int) (Math.random() * 2000000) + 1;
+            temp = String.valueOf(randInt);
+
+            textArea.setPromptText(temp);
+        }
+
+        System.out.println("Message: " + temp);
+
+        event.consume();
+    }
+
+    @FXML
+    private void handleExitButtonClicked() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/MainView.fxml")));
+        Scene newScene = new Scene(root);
+        App.getStage().setScene(newScene);
+    }
+
+    @FXML
+    private void handleGitButtonClicked(ActionEvent event) {
+        new Application() {
+            @Override
+            public void start(Stage stage) { }
+        }.getHostServices().showDocument("https://github.com/joseluisgomes/PITI/");
+        event.consume();
+    }
+
+    @FXML
+    private void handleOnlyIntCheckAction(ActionEvent event) {
+        if(integerCheck.isSelected()) { //println only here for testing purposes on the functionality of the checkbox
+            System.out.println("User only wants integers");
+            intCheckFlag = true;
+        } else {
+            System.out.println("User wants all chars");
+            intCheckFlag = false;
+        }
+        event.consume();
+    }
+
+}
---
+++ src/main/java/com/piti/controllers/ReceiverViewController.java
@@ -0,0 +1,31 @@
+package com.piti.controllers;
+
+import com.piti.App;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.util.Objects;
+
+public class ReceiverViewController {
+
+    @FXML
+    private void handleExitButtonClicked() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/MainView.fxml")));
+        Scene newScene = new Scene(root);
+        App.getStage().setScene(newScene);
+    }
+
+    @FXML
+    private void handleGitButtonClicked(ActionEvent event) {
+        new Application() {
+            @Override
+            public void start(Stage stage) { }
+        }.getHostServices().showDocument("https://github.com/joseluisgomes/PITI/");
+        event.consume();
+    }
+}
