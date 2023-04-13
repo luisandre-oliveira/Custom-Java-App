75d003477469ce03e51d7f1d51ab86ed674fb884
luisandre-oliveira <luisandre12100@gmail.com>
2023-04-13 12:24:13 +0100

    Addition of a new button to minimize the window

--- src/main/java/com/piti/controllers/MainViewController.java
+++ src/main/java/com/piti/controllers/MainViewController.java
@@ -10,6 +10,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
@@ -81,6 +82,12 @@ public class MainViewController implements Initializable {
             public void start(Stage stage) { }
         }.getHostServices().showDocument("https://github.com/joseluisgomes/PITI/");
         event.consume();
+    }
+
+    @FXML
+    private void handleMinimizeButtonClicked(ActionEvent event) {
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        stage.setIconified(true);
     }
 
     @FXML
